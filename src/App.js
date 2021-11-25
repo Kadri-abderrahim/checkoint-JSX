b905f14d67de6a54d2bc0dda2b298c70b8a1fa6b
Kadri-abderrahim <kdri.abderrahim@hotmail.com>
2021-11-25 22:53:57 +0100

    checkpoint-jsx

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+
+function App(){
+    return (
+      <div>
+
+        <div style={{ border: 'solid 1px black', maxWidth: '100vw' }}>
+          <h1 className="title red"> Votre nom ici </h1>
+          <br />
+
+          <br />
+          <img src="/imageInPublic.jpg" />
+        </div>
+        <contrôles de la largeur vidéo={320} height={240}>
+          <source src="myVideo.mp4" type="video / mp4" />
+        </contrôles>
+      </div>
+    );
+}
+export default App;
